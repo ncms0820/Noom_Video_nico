1d04f03444435f4ec2b361219b3c8b545deed35e
Minsu Cho <ncms0820@outlook.com>
2022-03-21 21:50:21 +0900

    manually constructed websocket v1

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,51 @@
-hello;
+// browser -> server // 이게 결국 브라우저인 것 이다.
+// connection 은 브라우저가 Server를 detect 한다
+const messageList = document.querySelector("ul");
+const messageForm = document.querySelector("#message");
+const nickForm = document.querySelector("#nick");
+
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+function makeMessage(type, payload) {
+  const msg = { type, payload };
+  return JSON.stringify(msg);
+}
+
+socket.addEventListener("open", () => {
+  console.log("Connected to Server");
+});
+
+socket.addEventListener("message", (message) => {
+  const li = document.createElement("li");
+  li.innerText = message.data;
+  messageList.append(li);
+});
+
+// 서버로 부터 받은 메시지 출력
+socket.addEventListener("message", (message) => {
+  console.log("New message: ", message.data);
+});
+
+socket.addEventListener("close", () => {
+  console.log("Disconnected from Server");
+});
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const input = messageForm.querySelector("input");
+  socket.send(makeMessage("new_message", input.value));
+  const li = document.createElement("li");
+  li.innerText = `You: ${input.value}`;
+  messageList.append(li);
+  input.value = "";
+}
+
+function handleNickSubmit(event) {
+  event.preventDefault();
+  const input = nickForm.querySelector("input");
+  socket.send(makeMessage("nickname", input.value));
+  input.value = "";
+}
+
+messageForm.addEventListener("submit", handleSubmit);
+nickForm.addEventListener("submit", handleNickSubmit);
